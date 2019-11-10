2e3adf568371cfff960f61d9a9f5df99c6e6226c
mateussilva98 <mateus_silva_12@hotmail.com>
2019-11-10 15:16:43 +0000

    bd connection & another stuff

---
+++ TPC8/premios/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    //pq retorna uma promise
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (err) {
+    console.log("Couldn't connect to MongoDB");
+  }
+};
+
+module.exports = connectDB;
